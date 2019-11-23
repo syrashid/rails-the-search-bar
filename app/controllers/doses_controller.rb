50333a003676391172a8ddd740a83517c836f128
Sy Rashid <sy.rashid.11@gmail.com>
2019-11-23 19:42:54 +1100

    FEAT: Users can add new doses to cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,23 @@
+class DosesController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktail/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.destroy(params[:id])
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
