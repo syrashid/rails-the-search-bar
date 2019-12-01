901820d51a8390f642425d13a330ba5ece7d2750
Sy Rashid <sy.rashid.11@gmail.com>
2019-12-01 00:19:43 -0500

    FEAT: Added the cocktail policy, dose and review policy to come

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,26 +2,26 @@ class CocktailsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    if params[:query].present?
-      @cocktails = Cocktail.search(params[:query])
-    else
-      @cocktails = Cocktail.all
-    end
+    @cocktails = policy_scope(Cocktail)
+    @cocktails = Cocktail.search(params[:query]) if params[:query].present?
   end
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    authorize @cocktail
     @review = Review.new
     @dose = Dose.new
   end
 
   def new
     @cocktail = Cocktail.new
+    authorize @cocktail
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.user = current_user
+    authorize @cocktail
     @cocktail.save ? (redirect_to cocktail_path(@cocktail)) : (render :new)
   end
 
---
+++ app/policies/cocktail_policy.rb
@@ -0,0 +1,20 @@
+class CocktailPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+end
