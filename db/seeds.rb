4e8beeeafbe0e8f82a94504ca490ab7bca252a66
Sy Rashid <sy.rashid.11@gmail.com>
2019-12-12 23:27:35 +0100

    FEAT: Gave seeds proper photographs

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,16 +29,34 @@ puts 'Completed Seeding Ingredients'
 puts 'Seeding Sample Cocktails'
 @tom = Cocktail.create(name: 'Tom Collins',
                        user: @sy,
-                       remote_photo_url: 'https://c7.uihere.com/files/948/354/439/cocktail-vodka-tom-collins-mojito-martini-ginger-thumb.jpg')
-Cocktail.create(name: 'Apple Martini', user: @sy)
-Cocktail.create(name: 'Long Island Ice Tea', user: @sy)
-Cocktail.create(name: 'Californication', user: @sy)
-Cocktail.create(name: 'Pina Colada', user: @sy)
-Cocktail.create(name: 'Margarita', user: @sy)
-Cocktail.create(name: 'Caipirinha', user: @sy)
-Cocktail.create(name: 'Mint Julep', user: @sy)
-Cocktail.create(name: 'Mai Tai', user: @sy)
-Cocktail.create(name: 'Mojito', user: @sy)
+                       remote_photo_url: 'https://i-love-png.com/images/333610_thumb.png')
+@appletini = Cocktail.create(name: 'Apple Martini',
+                user: @sy,
+                remote_photo_url: 'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_apple_martini-1.png')
+@liit = Cocktail.create(name: 'Long Island Ice Tea',
+                user: @sy,
+                remote_photo_url: 'https://cdn.shopify.com/s/files/1/0038/6049/6433/products/long-island-ice-tea_1080x.png?v=1569854167')
+@pina = Cocktail.create(name: 'Pina Colada',
+                user: @sy,
+                remote_photo_url: 'https://www.finestcall.com/wp-content/uploads/sites/3/2017/11/finestcallpinacolada.png')
+@marg = Cocktail.create(name: 'Margarita',
+                user: @sy,
+                remote_photo_url: 'https://www.cocktailartist.com/wp-content/uploads/2018/03/hellavamargarita.png')
+@caipi = Cocktail.create(name: 'Caipirinha',
+                user: @sy,
+                remote_photo_url: 'https://bartenderonduty.ch/wp-content/uploads/2018/08/132-1.png')
+@mintjul = Cocktail.create(name: 'Mint Julep',
+                user: @sy,
+                remote_photo_url: 'https://www.monin.com/us/media/catalog/product/cache/d8269468375af5c918c1809561ea76bf/B/l/Blueberry_Mint_Julep-1534128413-0.png')
+@maitai = Cocktail.create(name: 'Mai Tai',
+                user: @sy,
+                remote_photo_url: 'https://sensationaldrinks.com/media/1287/mai-tai.png')
+@mojito = Cocktail.create(name: 'Mojito',
+                user: @sy,
+                remote_photo_url: 'https://d32miag6ta013h.cloudfront.net/master_cocktails/185/en-gl/small/bacardi-mojito_sml_580x820..png')
+@russian = Cocktail.create(name: 'White Russian',
+                user: @sy,
+                remote_photo_url: 'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_white_russian-1.png')
 puts 'Completed Seeding Drinks'
 
 puts 'Making you a drink: Tom Collins'
@@ -57,6 +75,10 @@ puts '🍭 Adding Sugar'
 puts '🥤 Adding Club Soda'
 
 puts 'Mixing'
+
+puts puts 'Making you a drink: Apple Martini'
+
+puts '🍸'
 
 150.times { print '*' }
 
