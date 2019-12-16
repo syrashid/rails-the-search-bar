3d1bf289561aa1b3d916e3f5f9b1450a438e0a8a
Sy Rashid <sy.rashid.11@gmail.com>
2019-12-16 10:20:15 +0100

    FIX: Fixed styling of select2 box'

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  $('.select2').select2({
+    placeholder: 'Select Ingredient',
+    width: '100%'
+  });
 };
 
 export { initSelect2 };
